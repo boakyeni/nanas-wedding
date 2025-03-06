9dc3594e0b5ee4742f07459e68e44494b5f65073
Kojo <boakyeni@usc.edu>
2025-03-06 10:08:47 -0800

    changes again

--- src/app/components/InvitationCard.js
+++ src/app/components/InvitationCard.js
@@ -19,7 +19,7 @@ const InvitationCard = () => {
     }, []);
     return (
         <div className="relative w-full flex flex-col rounded-3xl bg-[url('/n_w2.jpeg')] bg-cover bg-center bg-no-repeat">
-            
+
             {/* Top Ticket Section */}
             <div className="flex flex-col items-center bg-gradient-to-r w-full aspect-[8/2] from-white via-amber-50/50 to-white rounded-t-3xl border border-amber-200 border-b-0 pt-2 shadow-sm">
                 <p className='mx-auto font-tangerine font-thin text-[#860384] text-6xl pt-4'>Save the Date</p>
@@ -27,17 +27,17 @@ const InvitationCard = () => {
             </div>
 
             {/* Bottom Ticket Section */}
-            <div className="[border-top-style:dashed] bg-gradient-to-r from-white via-amber-50/50 to-white rounded-b-3xl border border-amber-200 p-4 shadow-lg ">
+            <div className="[border-top-style:dashed] bg-gradient-to-r from-white via-amber-50/50 to-white rounded-b-3xl border border-amber-200 px-4 pb-4 shadow-lg ">
                 <div className="relative z-10">
                     {/* <div className="text-stone-400 text-xs tracking-wider mb-2">
                         BOARDING PASS
                     </div> */}
-                    <div className=" text-stone-600 text-sm font-montserrat font-thin [text-shadow:_0_1px_1px_rgb(251,191,36,0.2)]">
+                    <div className=" text-violet-950 text-sm font-montserrat [text-shadow:_0_1px_1px_rgb(251,191,36,0.2)]">
                         NOW BOARDING!
                     </div>
-                    <p className='mx-auto font-montserrat font-thin text-stone-500 text-md text-right'> <span className='font-tangerine text-stone-400 text-xl pr-2'>for</span> ACCRA</p>
-                    <div className="text-stone-400 text-xs uppercase tracking-wider mb-2 text-center pb-4 pt-1">
-                        JOIN US TO CELEBRATE OUR GREATEST ADVENTURE
+                    <p className='mx-auto font-montserrat text-violet-950 text-md text-right pb-4'> <span className='font-tangerine text-stone-400 text-xl pr-2'>for</span> ACCRA</p>
+                    <div className="text-violet-950 font-tangerine  text-2xl text-right">
+                        Abdul Wahab
                     </div>
                     {/* <div className="flex items-center gap-2 mb-2">
                         <div className="bg-stone-50 rounded-full p-1">
@@ -48,9 +48,9 @@ const InvitationCard = () => {
                             <Plane size={12} />
                         </div>
                     </div> */}
-                    <div className="relative flex justify-between text-2xl font-tangerine text-violet-950 mb-1 items-center">
-                        <p className="z-10">Nana Serwaa</p>
-                        <div className="absolute inset-0 flex justify-center items-center">
+                    <div className="relative flex justify-between text-2xl font-tangerine text-violet-950 pt-3 mb-1">
+                        {/* <p className="z-10">Nana Serwaa</p> */}
+                        <div className="absolute inset-5 flex justify-center items-center ml-5">
 
                             <div className="relative w-32 h-32 md:w-40 md:h-40 lg:w-48 lg:h-48">
                                 <Image
@@ -64,17 +64,22 @@ const InvitationCard = () => {
                                 />
                             </div>
                         </div>
-                        <p className="z-10">Abdul Wahab</p>
+                        {/* <p className="z-10">Abdul Wahab</p> */}
                     </div>
                     {/* <div className="text-2xl font-tangerine text-violet-950 mb-1 text-right">
                         Abdul Wahab
                     </div> */}
-                    <div className="flex justify-between text-stone-700 italic text-sm font-montserrat font-thin">
-                        <p>Destination:</p> <p>Accra, Ghana</p>
+                    <div className="text-violet-950 font-tangerine  text-2xl  text-left">
+                        Nana Serwaa
                     </div>
-                    <div className="flex justify-between text-violet-950 mb-4 text-sm italic font-montserrat font-thin">
-                        <p>Date:</p><p>January 3, 2026</p>
-                    </div>
+                    <div className='pt-6'>
+                        <div className="flex justify-between text-violet-950 italic text-sm font-montserrat ">
+                            <p>Destination:</p> <p>Accra, Ghana</p>
+                        </div>
+                        <div className="flex justify-between text-violet-950 mb-2 text-sm italic font-montserrat ">
+                            <p>Date:</p><p>January 3, 2026</p>
+                        </div>
+                    
 
                     <div className="text-center text-stone-400 text-xs mb-2">
                         FORMAL INVITATION TO FOLLOW
@@ -89,6 +94,7 @@ const InvitationCard = () => {
                                 style={{ opacity }}
                             />
                         ))}
+                    </div>
                     </div>
                 </div>
             </div>
