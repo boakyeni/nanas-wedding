dcf24eeb91d9cbd21945fda63a3d74634cc386d3
Kojo <boakyeni@usc.edu>
2025-11-04 20:39:49 -0800

    no rsvp

--- src/app/components/RSVPCircleWrapper.jsx
+++ src/app/components/RSVPCircleWrapper.jsx
@@ -5,6 +5,6 @@ import RSVPCircle from "./RSVPCircle";
 
 export default function RSVPCircleWrapper() {
   const pathname = usePathname();
-  if (pathname.startsWith("/rsvp") || pathname.startsWith("/checkin")) return null;
+  if (pathname.startsWith("/rsvp") || pathname.startsWith("/checkin") || pathname.startsWith("/invitation")) return null;
   return <RSVPCircle />;
 }
