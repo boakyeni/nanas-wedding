3937480818b1c62384848ccd7bc8ceeaaa04ec4e
Kojo <boakyeni@usc.edu>
2025-12-29 09:02:18 +0000

    no rsvp

--- src/app/rsvp/page.js
+++ src/app/rsvp/page.js
@@ -1,10 +1,17 @@
 'use client'
 import RSVP from "../components/RSVP"
+import { useEffect } from "react"
 
 const RSVPPage = () => {
+    useEffect(() => {
+    window.location.href = "/checkin";
+  }, []);
+
+  return null;
     return (
         <>
-            <RSVP/>
+        
+            {/* <RSVP/> */}
         </>
     )
 }
